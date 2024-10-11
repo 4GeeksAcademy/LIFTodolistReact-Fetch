ccf83d53066e6f535c1903e8c55978d2dc2118a9
Campeones1950 <168790271+Campeones1950@users.noreply.github.com>
2024-10-11 21:45:17 +0000

    LIF actualiza array y delete array

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = () => { 
+	const [todo, setTodo]= useState("");
+	const [todos, setTodos]= useState([]);
+
+	const deleteTodo = (indexToDelete) => {
+		setTodos(todos.filter((_, index) => index !== indexToDelete));
+	}; 
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container mt-5">
+
+			<input 
+			className="form-control"  
+			placeholder="What needs to be done" 
+			value={todo}
+			onChange={(e)=> setTodo(e.target.value)}
+
+			onKeyDown={(e) => 
+				e.code === "Enter" && todo.trim() !== ""
+				?(setTodos([...todos, todo]),
+					setTodo(""))
+					:null
+			
+		}
+			/>
+
+			<ul>
+				{todos.length >0
+				?todos.map((task, index)=> (
+					<li key = {index} className= "form-control d-flex justify-content-between align-items-center">{task}
+					<button className="btn btn-danger btn-sm ml-2"
+					onClick={() => deleteTodo(index)}
+					>
+                     				 				 
+					 x
+					</button>
+					
+					</li>
+
+				))
+				: <li> No task added</li>
+				}
+			</ul>
 		</div>
 	);
 };
